defa2236ae3e8e73b7303c727d71653fb5d8f3cd
Allen Shaw <allen@JoineryHQ.com>
2020-03-06 11:57:12 -0600

    Added FPPTAParticipantListing report.

---
+++ CRM/Fpptareports/Form/Report/Event/FPPTAParticipantListing.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Fpptareports_Form_Report_Event_FPPTAParticipantListing',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'FPPTA Event Participant Report (List)',
+      'description' => 'Event_FPPTAParticipantListing (com.joineryhq.fpptareports)',
+      'class_name' => 'CRM_Fpptareports_Form_Report_Event_FPPTAParticipantListing',
+      'report_url' => 'fpptareports/participantlisting',
+      'component' => 'CiviEvent',
+    ),
+  ),
+);
