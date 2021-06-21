32a41768098fd55d0ff64227c298bd2303767ef5
Allen Shaw <allen@JoineryHQ.com>
2021-06-21 16:16:51 -0500

    Correct label and description on report CRM_Fpptareports_Form_Report_Payment_Extra

--- CRM/Fpptareports/Form/Report/Payment/Extra.mgd.php
+++ CRM/Fpptareports/Form/Report/Payment/Extra.mgd.php
@@ -11,8 +11,6 @@ return [
       'version' => 3,
       'label' => 'FPPTA: Payment Extra',
       'description' => 'Custom for FPPTA: Payment details with extra data for soft credits, related orgs, and participant details',
-      'label' => 'Payment_Extra',
-      'description' => 'Payment_Extra (com.joineryhq.fpptareports)',
       'class_name' => 'CRM_Fpptareports_Form_Report_Payment_Extra',
       'report_url' => 'com.joineryhq.fpptareports/payment_extra',
       'component' => 'CiviContribute',
